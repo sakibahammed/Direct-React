21fa74a7e3434333b2ab2924a62d42a9e8198a59
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-03-29 14:59:38 +0600

    ema-jhon localsotrage

--- react-project/ema-jhon/ema-jhon/src/utilities/fakedb.js
+++ react-project/ema-jhon/ema-jhon/src/utilities/fakedb.js
@@ -1,4 +1,4 @@
-// use local storage to manage cart data
+ // use local storage to manage cart data
 const addToDb = id =>{
     let shoppingCart = {};
 
@@ -20,6 +20,18 @@ const addToDb = id =>{
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
 
+
+const getStoredCart = () =>{
+    let shoppingCart = {};
+
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+    return shoppingCart;
+}
+
 const removeFromDb = id =>{
     const storedCart = localStorage.getItem('shopping-cart');
     if(storedCart){
@@ -37,6 +49,7 @@ const deleteShoppingCart = () =>{
 
 export {
     addToDb, 
+    getStoredCart,
     removeFromDb,
     deleteShoppingCart
 }
