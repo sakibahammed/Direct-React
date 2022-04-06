143d563cbf83d3fa7cd33411ef65a072c8180c60
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-04-07 02:42:01 +0600

    chart in react

---
+++ react-rechart/react-rechart/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mylinechart from './Component/Mylinechart/Mylinechart';
+
+function App() {
+  return (
+    <div className="">
+      <Mylinechart></Mylinechart>
+    </div>
+  );
+}
+
+export default App;
