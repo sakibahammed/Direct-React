40a1e67f485287c50f566f254d7c2eff20390f92
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-04-17 13:55:39 +0600

    project return

--- react-project/ema-jhon/ema-jhon/src/App.js
+++ react-project/ema-jhon/ema-jhon/src/App.js
@@ -1,23 +1,18 @@
+
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./Component/About/About";
+
 
 import Header from "./Component/Header/Header";
-import Inventory from "./Component/Inventory/Inventory";
-import Order from "./Component/Orders/Order";
 import Shop from "./Component/Shop/Shop";
+
+
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Shop></Shop>}></Route>
-        <Route path="/shop" element={<Shop></Shop>}></Route>
-        <Route path="/orders" element={<Order></Order>}></Route>
-        <Route path='/inventory'element={<Inventory></Inventory>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-      </Routes>
+     <Shop></Shop>    
     </div>
   );
 }
